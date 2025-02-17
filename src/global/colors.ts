06377515467e090a4eaa6096331d04791d9316fa
italoCesarSilvaOliveira <italo.cs.oliveira@gmail.com>
2025-02-17 15:32:57 -0300

    feat: Configurações de fontes e cores finalizadas

---
+++ src/global/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  selected: '#005BA5',
+  primary: '#3498db',
+  secondary: '#FFC107',
+  white: '#ffffff',
+  black: '#000000',
+  gray: '#7f8c8d',
+  positive: '#4CAF50',
+  negative: '#FF0000',
+};
+
---
+++ src/global/fonts.ts
@@ -0,0 +1,6 @@
+// src/global/fonts.ts
+export const fonts = {
+  regular: 'Roboto-Regular',
+  bold: 'Roboto-Bold',
+  italic: 'Roboto-Italic',
+};

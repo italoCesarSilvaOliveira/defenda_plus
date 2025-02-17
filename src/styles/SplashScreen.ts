b10728279c6f9250e8db253baadbc608fd8968d3
italoCesarSilvaOliveira <italo.cs.oliveira@gmail.com>
2025-02-17 15:41:31 -0300

    feat: Criação do Arquivo de Estilo Da SplashScreen

---
+++ src/styles/SplashScreen.ts
@@ -0,0 +1,8 @@
+// src/styles/HomeScreenStyles.ts
+import { StyleSheet } from 'react-native';
+import { colors } from '../global/colors';
+import { fonts } from '../global/fonts';
+
+export const HomeScreenStyles = StyleSheet.create({
+});
+

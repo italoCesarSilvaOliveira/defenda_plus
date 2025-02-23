eda166a1542963be8e2bba03c7827e9a66d4e6c2
ednsoares <ednei5137@gmail.com>
2025-02-23 13:26:39 -0300

    Ajuste na tela de login.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { Stack } from "expo-router";
-
+import { Login } from "../screens/login";
 
 export default function RootLayout() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+
+  if (!isLoggedIn) {
+    return <Login onLogin={() => setIsLoggedIn(true)} />; 
+  }
+
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
-  )
+  );
 }
